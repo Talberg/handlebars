88702314dc7e7e88e6c42feed25e7469933bd570
Alexander Talberg <alexandermtalberg@gmail.com>
2020-02-01 11:23:56 -0700

    going back to old branch

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -2,6 +2,8 @@ var express = require("express");
 
 var router = express.Router();
 
+
+
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
@@ -17,7 +19,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.create([
+  burger.created([
     "name", "devoured"
   ], [
     req.body.name, req.body.devoured
@@ -32,7 +34,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  burger.update({
+  burger.updated({
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
@@ -47,7 +49,7 @@ router.put("/api/burgers/:id", function(req, res) {
 router.delete("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  burger.delete(condition, function(result) {
+  burger.deleted(condition, function(result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
