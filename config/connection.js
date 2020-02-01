1def90948c923dd3f80d154b997f2284c228837d
Alexander Talberg <alexandermtalberg@gmail.com>
2020-02-01 11:51:52 -0700

    reverted to the last working commit befor i sequelized re sequelized then made sure it worked

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,36 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Greblat2",
-  database: "burger_db"
-});
 
 // Make connection.
+
+
+// Export connection for our ORM to use.
+
+
+
+
+var mysql = require('mysql');
+
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = 
+    mysql.createConnection({
+     host: "localhost",
+     port: 3306,
+     user: "root",
+     password: "Greblat2",
+     database: "burger_db"
+   });
+};
+
+var mysql = require('mysql');
+
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -17,6 +38,4 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
-
-// Export connection for our ORM to use.
 module.exports = connection;
